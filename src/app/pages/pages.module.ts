50f4ca3acc2f9398a948154bf9b06c73b6f49779
pedro <pedrodiaz0487@gmail.com>
2023-04-16 07:33:05 -0300

    eliminacion tramites-ciudadano

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -11,15 +11,10 @@ import { InputTextModule } from 'primeng/inputtext';
 import { InputTextareaModule } from 'primeng/inputtextarea';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { TramitesPrincipalComponent } from './tramites-administrador/tramites-principal/tramites-principal.component';
-import { TramitesCiudadanoNuevoComponent } from './tramites-ciudadano/tramites-ciudadano-nuevo/tramites-ciudadano-nuevo.component';
-import { TramitesCiudadanoPrincipalComponent } from './tramites-ciudadano/tramites-ciudadano-principal/tramites-ciudadano-principal.component';
-import { TramitesCiudadanoAsignadosComponent } from './tramites-ciudadano/tramites-ciudadano-asignados/tramites-ciudadano-asignados.component';
-import { TramitesCiudadanoFinalizadosComponent } from './tramites-ciudadano/tramites-ciudadano-finalizados/tramites-ciudadano-finalizados.component';
 import { TramitesFinalizadosComponent } from './tramites-administrador/tramites-finalizados/tramites-finalizados.component';
 import { TramitesNuevoComponent } from './tramites-administrador/tramites-nuevo/tramites-nuevo.component';
 import { TramitesAsignadosComponent } from './tramites-administrador/tramites-asignados/tramites-asignados.component';
 import { TramitesNuevoslisComponent } from './tramites-administrador/tramites-nuevoslis/tramites-nuevoslis.component';
-import { TramitesCiudadanoNuevoslisComponent } from './tramites-ciudadano/tramites-ciudadano-nuevoslis/tramites-ciudadano-nuevoslis.component';
 import { DropdownModule } from 'primeng/dropdown';
 import { TramitesUsuarioAsignadosComponent } from './tramites-usuario/tramites-usuario-asignados/tramites-usuario-asignados.component';
 import { TramitesUsuarioFinalizadosComponent } from './tramites-usuario/tramites-usuario-finalizados/tramites-usuario-finalizados.component';
@@ -29,7 +24,6 @@ import { UsuariosListaComponent } from './usuarios/usuarios-lista/usuarios-lista
 import { CalendarModule } from 'primeng/calendar';
 import { PasswordModule } from 'primeng/password';
 import { BrowserModule } from '@angular/platform-browser';
-import { TramitesCiudadanoAdministrarComponent } from './tramites-ciudadano/tramites-ciudadano-administrar/tramites-ciudadano-administrar.component';
 import { TramitesUsuarioAdministrarComponent } from './tramites-usuario/tramites-usuario-administrar/tramites-usuario-administrar.component';
 import { CiudadanosAdministrarComponent } from './ciudadanos/ciudadanos-administrar/ciudadanos-administrar.component';
 import { CentroAdministrarComponent } from './centros-mediacion/centro-administrar/centro-mediacion-administrar.component';
@@ -71,22 +65,16 @@ import { TramitesAdministrarMediadorComponent } from './tramites-administrador/t
   ],
   declarations: [
     CiudadanosListaComponent,
-    TramitesPrincipalComponent,    
-    TramitesCiudadanoAsignadosComponent,
-    TramitesCiudadanoFinalizadosComponent,
-    TramitesCiudadanoNuevoComponent,
-    TramitesCiudadanoPrincipalComponent,
+    TramitesPrincipalComponent, 
     TramitesFinalizadosComponent,
     TramitesNuevoComponent,
     TramitesAsignadosComponent,
     TramitesNuevoslisComponent,
-    TramitesCiudadanoNuevoslisComponent,
     TramitesUsuarioAsignadosComponent,
     TramitesUsuarioFinalizadosComponent,
     TramitesAdministrarComponent,
     UsuariosListaComponent,
     UsuariosAdministrarComponent,
-    TramitesCiudadanoAdministrarComponent,
     TramitesUsuarioAdministrarComponent,
     CiudadanosAdministrarComponent,
     CentroAdministrarComponent,
