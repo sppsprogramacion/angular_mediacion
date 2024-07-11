81db387ae3379ffe66d17ad0486faaf194d24264
pedrodiaz0487 <pedrodiaz0487@gmail.com>
2024-07-11 19:36:41 -0300

    subir2

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  //URL_BASE: "https://sppsprogramacion.online"
-  URL_BASE: "http://172.17.40.100:3000"
+  URL_BASE: "https://sppsprogramacion.online"
+  //URL_BASE: "http://172.17.40.100:3000"
 };
