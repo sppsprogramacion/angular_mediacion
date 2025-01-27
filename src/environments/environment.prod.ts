930820f1f8110d7b2ddf09c00fff0a3e0a19f297
Pedro <TuCorreo@example.com>
2025-01-27 18:14:11 -0300

    cambio de ruta en enviroment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: true,
-  URL_BASE: "https://programacionspps.store/api"
+  URL_BASE: "http://bdmediacion.salta.gob.ar/api",
+  //URL_BASE: "https://programacionspps.store/api",
   //URL_BASE: "http://172.17.40.100:3000"
   //URL_BASE: "http://localhost:3000/api"
 };
