64549458a20578d49d82b1eac9d2f89fc91dee30
Pedro <TuCorreo@example.com>
2025-07-15 20:24:17 -0300

    correciones estilos en linea

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,33 +1,12 @@
 import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
-import { DashboardComponent } from './components/dashboard/dashboard.component';
-import { FormLayoutComponent } from './components/formlayout/formlayout.component';
-import { PanelsComponent } from './components/panels/panels.component';
-import { OverlaysComponent } from './components/overlays/overlays.component';
-import { MediaComponent } from './components/media/media.component';
-import { MessagesComponent } from './components/messages/messages.component';
-import { MiscComponent } from './components/misc/misc.component';
-import { EmptyComponent } from './components/empty/empty.component';
-import { ChartsComponent } from './components/charts/charts.component';
-import { FileComponent } from './components/file/file.component';
-import { DocumentationComponent } from './components/documentation/documentation.component';
-import { AppMainComponent } from './app.main.component';
-import { InputComponent } from './components/input/input.component';
-import { ButtonComponent } from './components/button/button.component';
-import { TableComponent } from './components/table/table.component';
-import { ListComponent } from './components/list/list.component';
-import { TreeComponent } from './components/tree/tree.component';
-import { CrudComponent } from './components/crud/crud.component';
-import { BlocksComponent } from './components/blocks/blocks.component';
-import { FloatLabelComponent } from './components/floatlabel/floatlabel.component';
-import { InvalidStateComponent } from './components/invalidstate/invalidstate.component';
-import { TimelineComponent } from './components/timeline/timeline.component';
-import { IconsComponent } from './components/icons/icons.component';
-import { LandingComponent } from './components/landing/landing.component';
 
+import { AppMainComponent } from './app.main.component';
+import { LandingComponent } from './components/landing/landing.component';
 import { ErrorComponent } from './components/error/error.component';
 import { NotfoundComponent } from './components/notfound/notfound.component';
 import { AccessComponent } from './components/access/access.component';
+
 import { CiudadanosListaComponent } from './pages/ciudadanos/ciudadanos-lista/ciudadanos-lista.component';
 import { LoginComponent } from './auth/login/login.component';
 import { RegistroComponent } from './auth/registro/registro.component';
@@ -137,48 +116,7 @@ import { DescargasTramitesAsignadosComponent } from './pages/descargas/descargas
                 ],
                 
             },
-            // {
-            //     path: 'pages', component: AppMainComponent,
-            //     children: [
-            //         {path: '', component: TramitesPrincipalComponent},
-            //         {path: 'home/principal', component: TramitesPrincipalComponent},
-
-            //         {path: 'admin/categoria/lista', component: CategoriasListaComponent},
-            //         {path: 'admin/categoria/administrar', component: CategoriaAdministrarComponent},
-                    
-            //         {path: 'admin/ciudadanos/administrar', component: CiudadanosAdministrarComponent},
-            //         {path: 'admin/ciudadanos/buscar', component: CiudadanosBuscarComponent},
-            //         {path: 'admin/ciudadanos/lista', component: CiudadanosListaComponent},
-            //         {path: 'admin/ciudadanos-usuarios/administrar', component: CiudadanoUsuarioComponent},
-                    
-            //         {path: 'admin/centro-mediacion/lista', component: CentrosMediacionListaComponent},
-            //         {path: 'admin/centro-mediacion/administrar', component: CentroAdministrarComponent},
-                    
-            //         {path: 'admin/usuarios/administrar', component: UsuariosAdministrarComponent},
-            //         {path: 'admin/usuarios/lista', component: UsuariosListaComponent},
-                    
-            //         {path: 'admin/tramites/administrar', component: TramitesAdministrarComponent},
-            //         {path: 'admin/tramites/administrar-med', component: TramitesAdministrarMediadorComponent},
-            //         {path: 'admin/tramites/asignados', component: TramitesAsignadosComponent},
-            //         {path: 'admin/tramites/finalizados', component: TramitesFinalizadosComponent},
-            //         {path: 'admin/tramites/nuevo', component: TramitesNuevoComponent},
-            //         {path: 'admin/tramites/nuevoslis', component: TramitesNuevoslisComponent},
-                                        
-            //         {path: 'ciudadano/principal', component: CiudadanoPrincipalComponent},
-            //         {path: 'ciudadano/cambiarcontrasenia', component: CiudadanoDatospersonalesComponent},
-            //         {path: 'ciudadano/datospersonales', component: CiudadanoDatospersonalesComponent},
-            //         {path: 'ciudadano/tramites/administrar', component: CiudadanoTramitesAdministrarComponent},
-            //         {path: 'ciudadano/tramites/finalizados', component: CiudadanoTramitesFinalizadosComponent},
-            //         {path: 'ciudadano/tramites/nuevo', component: CiudadanoTramitesNuevoComponent},
-            //         {path: 'ciudadano/tramites/nuevos', component: CiudadanoTramitesNuevoslistaComponent},
-                    
-            //         {path: 'usuario/tramites/administrar', component: TramitesAdministrarComponent},
-            //         {path: 'usuario/tramites/asignados', component: TramitesUsuarioAsignadosComponent},
-            //         {path: 'usuario/tramites/finalizados', component: TramitesUsuarioFinalizadosComponent},
-                                        
-            //     ],
-                
-            // },
+            
             {path:'pages/landing', component: LandingComponent},
             {path:'login', component: LoginComponent},
             {path:'login-admin-mediacion', component: LoginUsuarioComponent},
